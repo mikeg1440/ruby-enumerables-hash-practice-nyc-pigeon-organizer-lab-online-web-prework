abc97ad81abcd8566683f10e519538d53bac2bcc
Michael Gaudreau <gaudreaum01@gmail.com>
2019-08-30 15:26:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -58,7 +58,7 @@ def nyc_pigeon_organizer(data)
             
               puts ":::#{subkey.class}"
               puts pidgeons[name][newKey].class
-              puts "$$$" + pidgeons[name][newKey].to_s
+              puts "$$$" + pidgeons[name][newKey]
               puts "***"
               puts pidgeons[name][newKey]
             
