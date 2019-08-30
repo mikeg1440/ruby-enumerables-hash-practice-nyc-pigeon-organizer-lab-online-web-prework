0edb9c79f41621b729fc685a04bcbf6de569f005
Michael Gaudreau <gaudreaum01@gmail.com>
2019-08-30 05:18:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,6 +7,12 @@ def nyc_pigeon_organizer(data)
   data.each do |key, attrs|
     puts "Key: #{key}\nAttrs: #{attrs.values}"
     
+    names = attrs.values
+    
+    names.each do |name|
+      pidgeons[name] = {}
+    end
+    
   end
  
 end
