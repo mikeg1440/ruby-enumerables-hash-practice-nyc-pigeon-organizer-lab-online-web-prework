7381bbbb25f7327ff3301b981cc350749d2fdb10
Michael Gaudreau <gaudreaum01@gmail.com>
2019-08-30 05:21:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   
   pidgeons = {}
   
-  puts "DATA: #{data[:gender]}"
+  puts "DATA: #{data[:gender].values}"
 
   data.each do |key, attrs|
     puts "Key: #{key}\nAttrs: #{attrs.values}"
