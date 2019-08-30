0bc6a138935c503d12ea5f3242c4657a0482f6d7
Michael Gaudreau <gaudreaum01@gmail.com>
2019-08-30 06:01:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -43,7 +43,7 @@ def nyc_pigeon_organizer(data)
               puts "***"
               puts pidgeons[name][":#{key}"]
             else
-              pidgeons[name][":#{key}"] += [newKey]
+              pidgeons[name][":#{key}"].push(newKey)
             end
             
           end
